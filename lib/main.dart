b3f95bcddcf1505a1a437ed2d097b2118fdffc08
Aktilek <actilek.m@livin.kz>
2024-10-23 19:21:13 +0500

    а

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:aktau_go/utils/utils.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import './router/router.dart';
 import './app.dart';
@@ -20,6 +22,9 @@ Future<void> main() async {
   MapboxOptions.setAccessToken(
       'sk.eyJ1IjoidmFuZGVydmFpeiIsImEiOiJjbTA1azhkNjEwNDF2MmtzNHA0eWJ3eTR0In0.cSGmIeLW1Wc44gyBBWJsYA');
   await initDi(Environment.prod);
+  FirebaseMessaging.instance.requestPermission(
+    provisional: true,
+  );
 
   await inject<NotificationInteractor>().setupFirebaseConfig();
   runApp(
